1a176fe28a837dcc61fa17dc2f21138eeffc6df2
marshall405 <marshall.slemp@gmail.com>
2020-06-25 18:09:04 -0500

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 
 export default class Cell extends Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {
-      color: this.props.color
+      color: this.props.color,
+      border: ''
     }
   }
-  
+
+  handleClick = () => {
+    this.setState({
+      color: this.props.selectedColor
+    })
+  }
+
+  handleMouseEnter = () => {
+    this.setState({
+      border: `1px solid ${this.props.selectedColor}`
+    })
+  }
+
+  handleMouseLeave = () => {
+    this.setState({
+      border: ''
+    })
+  }
+
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div className="cell" style={{ backgroundColor: this.state.color, border: this.state.border }} onClick={this.handleClick} onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
       </div>
     )
   }
-  
+
 }
